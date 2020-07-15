806325aa2be058477c4114d731d39478d3659e42
Comyune Technologies <comyune@gmail.com>
2020-07-15 12:17:37 +0100

    Add cors support to S3

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -6,4 +6,12 @@ resource "aws_s3_bucket" "sebthecat_blog" {
     index_document = "index.html"
     error_document = "error.html"
   }
+
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["GET"]
+    allowed_origins = ["https://sebthecat.com", "https://www.sebthecat.com"]
+    expose_headers  = ["ETag"]
+    max_age_seconds = 3600 // 1 hour
+  }
 }
