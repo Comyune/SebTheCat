2abe135b18671550cf9966f31169b41b0bcf5519
Comyune Technologies <comyune@gmail.com>
2020-08-02 23:39:33 +0100

    Fix CORS rules on Cloudfront distribution

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -10,7 +10,15 @@ resource "aws_s3_bucket" "sebthecat_blog" {
   cors_rule {
     allowed_headers = ["*"]
     allowed_methods = ["GET"]
-    allowed_origins = ["https://sebthecat.com", "https://www.sebthecat.com"]
+    allowed_origins = ["https://sebthecat.com"]
+    expose_headers  = ["ETag"]
+    max_age_seconds = 3600 // 1 hour
+  }
+
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["GET"]
+    allowed_origins = ["https://www.sebthecat.com"]
     expose_headers  = ["ETag"]
     max_age_seconds = 3600 // 1 hour
   }
