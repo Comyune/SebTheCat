3988924bac1f397813ee837adc74dcc44c1c0ca3
Comyune Technologies <comyune@gmail.com>
2020-08-17 22:47:44 +0100

    Update run script

--- scripts/run_server.sh
+++ scripts/run_server.sh
@@ -3,9 +3,8 @@
 hugo serve \
   --disableFastRender \
   --baseURL "http://192.168.0.203:1313" \
-  --bind "0.0.0.0" \ 
-  --debug \
   --log \
   --verbose \
   --verboseLog \
-  --navigateToChanged
+  --navigateToChanged \
+  --bind "0.0.0.0"
